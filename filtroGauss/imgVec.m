5ec73bce425f45fea0f20c40ee9f926078de4c65
Leonardo1133 <42939877+Leonardo1133@users.noreply.github.com>
2020-05-07 12:26:16 -0300

    comentado

--- filtroGauss/imgVec.m
+++ filtroGauss/imgVec.m
@@ -1,29 +1,23 @@
-function [ in_rna ] = ImVector( im_ruido )
-%transforma imagen con ruido a matriz 9x64k para entrada a la red
+% Transforma imagen 255x255 a matriz 9x64k para entrada a la red
+function[in_rna] = ImVector(im_ruido)
 a = 0;
 b = 0;
-for i = 2:255
-    
+for i = 2:255   
     for j = 2:255
         b = im_ruido(i-1:i+1,j-1:j+1);         % extrae un pixel y sus vecinos 3x3
-        T = transpose(b);                      % o T = ans'  %transpone la porcion 3x3 , muy necesario xq la prox fc vectoriza columnas y yo quiero filas x el algoritmo de wan
-        M = reshape(T,[],1);                   % forma un vector columna con 3x3 quedando 9x1
-        
+        T = transpose(b);                      % transpone la porcion 3x3
+        M = reshape(T,[],1);                   % forma un vector columna con T, quedando M = 9x1     
         if a == 0
             in_rna = M;
-        end
-        
+        end      
         if a > 0
-            in_rna = horzcat(in_rna,M);      %concatena en una matriz c/vector p/ingresar a la rna
-        end
-        
-        a = a + 1; 
-        
+            in_rna = horzcat(in_rna,M);        % concatena en una matriz c/vector p/ingresar a la red
+        end       
+        a = a + 1;        
     end
 end 
 a = 0; 
 T = 0; 
 M = 0; 
 ans = 0;
-
 end
